604c0c4171e034a03ac4a9955f94a1338c0992c6
sujalcharati <sujalc024@gmail.com>
2024-10-28 13:27:24 +0530

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,48 @@
-import Navbar from "./Components/Navbar";
+import Home from "./Components/Home";
 import "./App.css";
+import About from "./Components/About";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Services from "./Components/Services";
+import Work from "./Components/work";
+import Carrer from "./Components/Carrer";
+import Contact from "./Components/Contact";
+const  App = ()=> {
+  return (
+    <BrowserRouter>
+      <nav className="flex justify-center">
+        <ul className="flex list-none p-0">
+          <li className="mx-2">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="mx-2">
+            <Link to="/About">About us</Link>
+          </li>
+          <li className="mx-2">
+            <Link to="/services">Services</Link>
+          </li>
+          <li className="mx-2">
+            <Link to="/work">Our work</Link>
+          </li>
+          <li className="mx-2">
+            <Link to="/carrers">Carrers</Link>
+          </li>
+          <li className="mx-2">
+            <Link to="/contact">Contact us</Link>
+          </li>
+        </ul>
+      </nav>
 
-function App() {
-  return <div>
-    <Navbar/>
-  </div>;
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/work" element={< Work/>} />
+        <Route path="/carrers" element={<Carrer />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App;
---
+++ src/Components/Carrer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Carrer = ()=>{
+    return(
+        <div>
+           <h1> carrer </h1>
+        </div>
+    )
+}
+export default Carrer
